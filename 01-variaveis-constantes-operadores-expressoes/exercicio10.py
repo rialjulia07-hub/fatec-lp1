ccfb570b8557ee37e245c8016962959e2f993917
Júlia <rialjulia07@gmail.com>
2026-08-12 14:59:44 -0300

    Exercício 10

--- 01-variaveis-constantes-operadores-expressoes/exercicio10.py
+++ 01-variaveis-constantes-operadores-expressoes/exercicio10.py
@@ -3,3 +3,11 @@
 # média aritmética entre elas e exiba o resultado com print(). Em
 # seguida, utilizando um operador relacional, exiba também se a média é
 # maior ou igual a 6 (o resultado será um valor booleano, True ou False).
+
+nota1 = float(input('Digite a nota 1: '))
+nota2 = float(input('Digite a nota 2: '))
+nota3 = float(input('Digite a nota 3: '))
+
+media = nota1 + nota2 + nota3 / 3
+print(media)
+print(media >= 6)
