0d6dfd455610dfb19212b75c16596488568e281f
Júlia <rialjulia07@gmail.com>
2026-08-12 14:39:50 -0300

    Exercício 04

--- 01-variaveis-constantes-operadores-expressoes/exercicio04.py
+++ 01-variaveis-constantes-operadores-expressoes/exercicio04.py
@@ -2,3 +2,11 @@
 # Declare uma constante PI = 3.14159 e uma variável raio recebendo o valor
 # 5. Calcule a área de um círculo (área = PI * raio ** 2) e exiba o
 # resultado com print().
+
+PI = 3.14159
+raio = 5
+area = PI * raio ** 2
+
+print(area)
+print(f'{area:.2f}')
+print(round(area, 2))
