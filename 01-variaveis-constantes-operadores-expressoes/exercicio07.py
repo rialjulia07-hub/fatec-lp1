e74811ed87b8450190d52103235813536c8519ac
Júlia <rialjulia07@gmail.com>
2026-08-12 14:43:34 -0300

    Exercício 07

--- 01-variaveis-constantes-operadores-expressoes/exercicio07.py
+++ 01-variaveis-constantes-operadores-expressoes/exercicio07.py
@@ -1,3 +1,6 @@
 # Exercício 07
 # Peça ao usuário uma palavra utilizando input(). Utilize a função len()
 # para descobrir e exibir a quantidade de caracteres dessa palavra.
+
+palavra = input('Digite uma palavra: ')
+print(len(palavra))
