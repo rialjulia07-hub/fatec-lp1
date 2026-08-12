9ba13794ff26cc5a7bc30ebcf8a71a5550cc02ff
Júlia <rialjulia07@gmail.com>
2026-08-12 14:41:09 -0300

    Exercício 05

--- 01-variaveis-constantes-operadores-expressoes/exercicio05.py
+++ 01-variaveis-constantes-operadores-expressoes/exercicio05.py
@@ -2,3 +2,11 @@
 # Peça ao usuário dois números inteiros utilizando input(), convertendo-os
 # com a função int(). Exiba a soma, a subtração, a multiplicação e a
 # divisão entre eles.
+
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
