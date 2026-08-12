0532f3398f98b6292487f4f7cc995f2d5f4b46be
Júlia <rialjulia07@gmail.com>
2026-08-12 13:55:40 -0300

    exercício 01

--- 01-variaveis-constantes-operadores-expressoes/exercicio01.py
+++ 01-variaveis-constantes-operadores-expressoes/exercicio01.py
@@ -2,3 +2,7 @@
 # Declare uma variável com o seu nome e uma variável com a sua idade.
 # Utilize a função print() para exibir as duas informações em uma única
 # mensagem, no formato: "Meu nome é <nome> e eu tenho <idade> anos."
+
+nome = "Júlia" #STR
+idade = 18 #INT
+print('Meu nome é',nome, 'e tenho', idade, 'anos')
