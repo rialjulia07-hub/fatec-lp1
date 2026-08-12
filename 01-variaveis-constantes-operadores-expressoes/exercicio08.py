ad6f16f76ba2cde27c71ef840c6176e8118d14fd
Júlia <rialjulia07@gmail.com>
2026-08-12 14:44:40 -0300

    Exercício 08

--- 01-variaveis-constantes-operadores-expressoes/exercicio08.py
+++ 01-variaveis-constantes-operadores-expressoes/exercicio08.py
@@ -2,3 +2,13 @@
 # Peça ao usuário dois números utilizando input(). Utilize operadores
 # relacionais (>, <, ==, >=, <=, !=) para comparar os dois valores e exiba
 # o resultado de cada comparação.
+
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+print(a == b)
+print(a != b)
